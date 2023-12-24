2416877d1f3221bd60437772cf36be5433d878e4
jaatmotolove <125938254+jaatmotolove@users.noreply.github.com>
2023-12-25 01:28:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/jaatmotolove/offlinejaat",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Pataka")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Chiku")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
